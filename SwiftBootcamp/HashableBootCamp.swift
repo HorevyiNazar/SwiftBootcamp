1834c96621ca3ea7956f4a1cccd73f7e4fabf25c
HorevyiNazar <goreviynazar@gmail.com>
2023-07-12 14:09:18 +0300

    Hashable

---
+++ SwiftBootcamp/HashableBootCamp.swift
@@ -0,0 +1,44 @@
+//
+//  HashableBootCamp.swift
+//  SwiftBootcamp
+//
+//  Created by Назар Горевой on 12.07.2023.
+//
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
+struct HashableBootCamp: View {
+    
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "One"),
+        MyCustomModel(title: "Two"),
+        MyCustomModel(title: "Three"),
+        MyCustomModel(title: "Four"),
+        MyCustomModel(title: "Five"),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 40) {
+                ForEach(data, id: \.self) { item in
+                    Text(item.title)
+                        .font(.headline)
+                }
+            }
+        }
+    }
+}
+
+struct HashableBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        HashableBootCamp()
+    }
+}
